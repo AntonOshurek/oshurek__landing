dde4aede34a971ebcc9f918b29733eb6fabeffa3
AntonOshurek <oshurekweb@gmail.com>
2021-11-20 22:33:20 +0100

    add scroll top function

---
+++ source/scripts/blocks/scrollTopButton.js
@@ -0,0 +1,8 @@
+export default function scrollTopButton() {
+  const topButton = document.querySelector('.footer-copyright__button-top');
+
+  topButton.addEventListener('click', () => {
+    window.scrollTo(0,0);
+  });
+
+}
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,10 @@
 import portfolioSlider from './blocks/portfolio-lider';
 import navigation from './blocks/navigation';
+import scrollTopButton from './blocks/scrollTopButton';
 
 window.addEventListener('DOMContentLoaded', () => {
   portfolioSlider();
   navigation();
+  scrollTopButton();
 });
 
