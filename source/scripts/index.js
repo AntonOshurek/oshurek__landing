d9470bd1889ce5cba2c6c69d9c23dfc6731444bc
AntonOshurek <oshurekweb@gmail.com>
2021-11-25 22:10:28 +0100

    code correcting

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,4 @@
-import portfolioSlider from './blocks/portfolio-lider';
+import portfolioSlider from './blocks/portfolio-slider';
 import navigation from './blocks/navigation';
 import scrollTopButton from './blocks/scrollTopButton';
 import contactMeModal from './blocks/contact-me-modal';
