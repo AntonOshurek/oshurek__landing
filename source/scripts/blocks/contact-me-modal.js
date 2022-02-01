8bae5502b5bdf71021ed86bed3fef10b67c55cc0
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-01 13:38:22 +0100

    add mail func

--- source/scripts/blocks/contact-me-modal.js
+++ source/scripts/blocks/contact-me-modal.js
@@ -35,7 +35,6 @@ export default function contactMeModal() {
   function closeModal () {
     modal.classList.remove('contact-modal--open');
     body.classList.remove('body--scrolloff');
-
     modalTrigerBtn.focus();
   }
 }
