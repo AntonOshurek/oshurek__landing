6e14fedcbb01cb8ecdd0db8c73d11241169ddd4c
AntonOshurek <oshurekweb@gmail.com>
2022-02-02 22:02:39 +0100

    add php sent meil logick

--- source/sentMeil.php
+++ source/sentMeil.php
@@ -3,8 +3,24 @@
 
 
 if (isset($_POST)) {
-  print("<br>Email: " . $_POST['email']);
+
+  print("<br>Email: " . $_POST['emeil']);
+  print("<br>Сообщение: " . $_POST['title']);
   print("<br>Сообщение: " . $_POST['text']);
+
+  $meil = $_POST['emeil'];
+
+    $to = "<oshurekweb@gmail.com>" ; 
+
+    $subject = $_POST['title']; 
+
+    $message = $_POST['text'];
+
+    $headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
+    $headers .= "From: " . $meil; 
+    $headers .= "Reply-To: reply-to@example.com\r\n"; 
+
+    mail($to, $subject, $message, $headers); 
 }
 
 ?>
